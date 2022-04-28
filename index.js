60ad9b3ef3439441f32dce4fd810c5e68cf066c0
Aluno <felipemalicheski@gmail.com>
2022-04-27 21:57:31 -0300

    adicionando o metodo post

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const { getRepository } = require('typeorm');
+/*
+    Caso dar erro, rodar no terminal:
+        yarn add reflect-metadata
+    e colocar no topo do código:
+        require("reflect-metadata")
 
+*/
 const database = require("./src/database");
 const User = require('./src/entity/User');
 database()
@@ -29,6 +35,17 @@ app.get("/users", async (req, res) =>{
     return res.status(200).json({usuarios})
 })
 
+app.post("/users", async(req, res) => {
+    const{EMAIL, SENHA, NOME} = req.body
+    console.log(EMAIL,SENHA,NOME)
+    
+    //return res.json({EMAIL, SENHA, NOME});
+    let userRepository = getRepository("User")
+
+    const usuario = await userRepository.create();
+    return res.status(200).json({usuario})
+    
+})
 app.listen(3333, () => {
     console.log("mensagem fofa >-<")
 })
