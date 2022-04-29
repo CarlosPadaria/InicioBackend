2825e5baa06f92f4fb1105c5b1c89da046d8bdaa
Aluno <felipemalicheski@gmail.com>
2022-04-28 21:54:12 -0300

    atualizando o patch de senha do usuario

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getRepository } = require('typeorm');
+const { getRepository, Repository } = require('typeorm');
 /*
     Caso dar erro, rodar no terminal:
         yarn add reflect-metadata
@@ -14,7 +14,6 @@ database()
 let app = express();
 
 app.use(express.json())
-
 app.get("/", (req, res) =>{
     //req.body
     if(true)
@@ -31,21 +30,90 @@ app.get("/users", async (req, res) =>{
     //req.body
     let userRepository = getRepository("User");
     
+
     const usuarios = await userRepository.find();
+
     return res.status(200).json({usuarios})
+
 })
 
 app.post("/users", async(req, res) => {
-    const{EMAIL, SENHA, NOME} = req.body
-    console.log(EMAIL,SENHA,NOME)
+    const{EMAIL, SENHA, NOME, IDUSUARIO} = req.body
+    //console.log(req.body)
     
     //return res.json({EMAIL, SENHA, NOME});
     let userRepository = getRepository("User")
 
-    const usuario = await userRepository.create();
-    return res.status(200).json({usuario})
+    let VerificarContaJaCadastrada = await userRepository.findOne({
+        where: [
+            {IDUSUARIO: `${IDUSUARIO}`}
+        ]
+    })
+    console.log(VerificarContaJaCadastrada);
+
+    if(VerificarContaJaCadastrada === null)
+    {
+        const usuario = await userRepository.insert(
+            req.body
+        );
+
+        return res.status(200).json({usuario})
+    }
+    else
+    {
+        return res.status(400).json({message:"Conta ja cadastrada"});
+    }
+   // console.log(VerificarContaJaCadastrada)
+    
     
 })
+
+app.get("/users/:IDUSUARIO", async (req, res) =>{
+    console.log(req.params);
+    
+    let userRepository = getRepository("User")
+
+    let usuario = await userRepository.findOne({
+        where: [
+            {IDUSUARIO: req.params.IDUSUARIO}
+        ]
+    })
+   
+    if (usuario === null)
+    {
+        return res.status(400).json({message:"Usuário não encontrado!"})
+    }
+    else
+    {
+        return res.status(200).json({usuario})
+    }
+})
+
+app.patch("/users/:IDUSUARIO", async (req, res) =>{
+    console.log(req.params);
+    
+    let userRepository = getRepository("User")
+
+    let usuario = await userRepository.findOne({
+        where: [
+            {IDUSUARIO: req.params.IDUSUARIO}
+        ]
+    })
+    let alterarSenha
+    if (usuario === null)
+    {
+        return res.status(400).json({message:"Usuário não encontrado!"})
+    }
+    else
+    {
+        alterarSenha = await userRepository.update(
+            req.params.IDUSUARIO, {SENHA: `${req.body.SENHA}`}
+        )
+        usuario.SENHA = req.body.SENHA
+        return res.status(200).json({usuario})
+    }
+})
+
 app.listen(3333, () => {
     console.log("mensagem fofa >-<")
 })
